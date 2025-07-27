c7aaa91c006f531fa313cac5572f2de596f72366
vasq-hub <162423202+vasq-hub@users.noreply.github.com>
2025-07-27 10:18:33 +1000

    Update modulebot.py

--- modulebot.py
+++ modulebot.py
@@ -3,13 +3,13 @@ from assets.antispam import antispam
 
 @antispam
 async def bot_reply(message: types.Message):
-    if message.text.lower() in ("бот", "bot"):
-        await message.reply("✅ На месте! 😊")
+    if message.text and message.text.lower() in ['бот', 'bot']:
+        await message.reply('? На месте! ??')
 
 def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(bot_reply, lambda m: m.text and m.text.lower() in ("бот", "bot"))
+    dp.register_message_handler(bot_reply, lambda message: message.text and message.text.lower() in ['бот', 'bot'])
 
 MODULE_DESCRIPTION = {
-    'name': '✅ Ответ на вызов',
-    'description': 'Отвечает "На месте", когда пользователь пишет "бот"'
+    'name': '? Ответ бота',
+    'description': 'Отвечает на слова "бот" и "bot"'
 }
